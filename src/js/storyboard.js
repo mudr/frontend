4bf98097adb1a427eacf9535891b31895c7e7fd5
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-04-16 18:26:23 -0400

    storyboard save

--- src/js/storyboard.js
+++ src/js/storyboard.js
@@ -4,7 +4,23 @@ import { ajax } from 'jquery';
 // import Happy from '.material-ui/lib/svg/social/sentiment-satisfied';
 
 export default class Storyboard extends Component {
-	
+	constructor (...args) {
+		super(...args);
+		this.state {}
+	}
+
+
+
+
+
+
+
+
+ 		render () {
+		return(
+			<div className="storyboard-wrapper">
+				<div className="main-body-div">
+					<h3>Take in Dem' Good 
 
 
 
@@ -14,8 +30,8 @@ export default class Storyboard extends Component {
 
 
 
-	// render () {
-	// 	return(
+
+			</div>
 
 
 
--- src/js/submit.js
+++ src/js/submit.js
@@ -1,58 +1,29 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import { hashHistory } from 'react-router';
 import SSF from 'react-simple-serial-form';
 import { ajax } from 'jquery';
 
 
-// title, content
+
 
 export default class Submission extends Component {
-	let currentuser = cookie.getJSON('user');
+	
 
-	  dataHandler(data) {
-
-
-   		ajax({
-       		url: 'http://mudr.herokuapp.com/posts/create',
-       		type: 'POST',
-       		data: data,
-	       	dataType: 'json',
-	       	headers: {
-	        	  'X-Access-Token': currentuser.auth_token
-		       	}
-
-	     }).then( () => {
-	     	hashHistory.replace("/storyboard");
-
-	     });
-  }
+	// onData() write Dat function
 
 
-	render(){
-		return(
-			 <div className="submit-story-wrapper">
-        		<h1>Share Your Story</h1>
-
-        		<SSF onData={this.dataHandler}>
-
-        			<div className="submit-title">
-           			 <input name="title" placeholder="title of your story" type="text"/>
-         			</div>
-
-          			<div className="submit-content">
-           			 <input name="content" placeholder="tell us whatsup" type="text"/>
-         			</div>
-        
-         		 <button>Submit Your Story</button>
-
-       		   </SSF>
+ 	render(){
+ 		return(
+ 			<div className="submission-wrapper">
+ 				
+ 				
+ 			</div>
 
 
-     		 </div>
+// // <SSF onData={}>
 
+// // </SSF>
 
-
-			)
-	}
+// 			)
+// 	}
 }
