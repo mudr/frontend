71addb68d7688264a0561e7db01a6af273ea1b74
FIRST_NAME LAST_NAME <joshuadiboll@gmail.com>
2016-04-17 11:03:12 -0400

    fixing conflicts

--- src/js/storyboard.js
+++ src/js/storyboard.js
@@ -47,10 +47,6 @@ export default class Storyboard extends Component {
 						<div className="story-box-title"> Story Title</div>
 						<div className="story-box-title">
 
-<<<<<<< HEAD
-
-=======
->>>>>>> b6e0b8c7de867d38e03fcc2ab5d73440d6733eb5
 						</div>
 					</div>
 				</div>
