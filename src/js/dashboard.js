7d26bf0485bd1290e033fc1659d3bd1bcd4fb5bd
BRITTANY ANDREWS <brittanyandrewsnyc@gmail.com>
2016-04-16 22:24:52 -0400

    styled the dashboard

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -12,17 +12,36 @@ export default class Dashboard extends Component {
 	render(){	
 
 		return(
+
 			<div className="mood-select">
+
+				<span> How are you feeling today?</span>
+		
+				<div className="wrapper1">
 				<div className="happy-link">
-					<Link to="/storyboard">I am HAPPY</Link>
+					<Link to="/storyboard">
+					<div className="hidden1">HAPPY</div>
+					<img src="http://www.forex21.com/wp-content/uploads/2015/05/happy-person.png" 
+					alt="happy"/>
+
+					</Link>
 				</div>
+				</div>
+
+
+				<div className="wrapper2">
 				<div className="sad-link">
-					<Link to="/submission">I am SAD</Link>
+					<Link to="/submission">
+					<div className="hidden2">SAD</div>
+					<img src="http://orig06.deviantart.net/d439/f/2012/320/b/8/obama_frown_render_by_kernelpanicx-d5l5f1z.png" 
+					alt="happy"/>
+
+					</Link>
+
 				</div>
-			</div>
+				</div>
 
-
-
+			  </div>
 
 
 		)
